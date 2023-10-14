568a1d6892187165bb9b3a0f8a250e6da7ff7cfb
Zhandarbek <105858944+Zhandr678@users.noreply.github.com>
2023-10-14 10:21:45 +0600

    Update Library.sol

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -1,2 +1,27 @@
 pragma solidity ^0.8.0;
 
+contract Library
+{
+    struct Book
+    {
+        uint id;
+        string name;
+        uint year;
+        string author;
+        bool finished;
+    }
+
+    Book[] private bookList;
+
+    mapping(uint256 => address) bookToOwner;
+
+    event AddBlock(address recipient, uint bookId);
+
+    function addBlock(string memory name, uint year, string memory author, bool finished) external
+    {
+        uint bookId = bookList.length;
+        bookList.push(Book(bookId, name, year, author, finished));
+        bookToOwner[bookId] = msg.sender;
+        emit AddBlock(msg.sender, bookId);
+    }
+}

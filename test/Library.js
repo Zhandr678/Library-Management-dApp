f38d219f71320b56db7144a1c9b9f58f91e30140
z4deas <mybigduck1337@gmail.com>
2023-11-04 00:39:00 +0600

    Updated tests

--- test/Library.js
+++ test/Library.js
@@ -1,7 +1,7 @@
 
 
-const {expect} = require("Chai");
-const {ethers} = require("ethers");
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
 
 
 function getRandomInt(min,max){
@@ -88,12 +88,22 @@ describe("Library Contract", function(){
             const booksFromChain = await library.getUnfinishedBooks();
             expect(booksFromChain.length).to.equal(NUM_UNFINISHED_BOOK);
             verifyBookList(booksFromChain, unfinishedBookList);
-        })
+        });
         it("Should return the correct finished books", async function(){
             const booksFromChain = await library.getFinishedBooks();
             expect(booksFromChain.length).to.equal(NUM_FINISHED_BOOK);
             verifyBookList(booksFromChain, finishedBookList);
+        });
+    });
+
+    describe("Set Finished", function(){
+        it("Should emit SetFinished event", async function(){
+            const BOOK_ID = 0;
+            const BOOK_FINISHED = true;
+
+            await expect(library.setFinished(BOOK_ID, BOOK_FINISHED)).to.emit(library, 'SetFinished').withArgs(BOOK_ID, BOOK_FINISHED);
         })
-    })
+    });
+
 
 });
